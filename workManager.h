541b2524e0476453fed27e9c2c850789995e40cb
YiboDan <42585395+danyibo@users.noreply.github.com>
2020-12-01 14:20:32 +0800

    Add files via upload
    2020年12月1日

---
+++ workManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class WorkerManager {
+public:
+	WorkerManager();
+	~WorkerManager();
+	
+	// 功能1：显示菜单
+	void Show_Menu();
+
+	// 功能2：退出
+	void exitSystem();
+};
+
